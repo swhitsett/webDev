77b6f44ed917abe71e783d69b31c266993a4031e
swhitsett <kongzero32@gmail.com>
2014-09-04 09:51:46 -0700

    some fuctionality

--- rfind.rb
+++ rfind.rb
@@ -1,1 +1,11 @@
-puts 'first-commit'
+a = ARGV[0] 
+  puts a 
+#end 
+IO.popen('grep a *') do |cmd_stdout_stream|
+  if cmd_stdout_stream.eof? == false	  # if the command has some output
+    cmd_stdout_stream.each do |filename|  # print every line in the commands output
+      puts 'file: ' + filename
+    end
+  end
+end
+
