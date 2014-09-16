6cec03f7f142d9046fb935adf3ae9408d8cb3592
swhitsett <kongzero32@gmail.com>
2014-09-16 01:22:59 -0700

    added word deletion

--- p2/hangman.rb
+++ p2/hangman.rb
@@ -4,26 +4,39 @@
 # 
 
 # create a functuon to remove words from this array.
+# to choose a word from the dictionary 
+num_of_attempts = 10
+guess_array = Array.new
+
 dictionary =File.open("wordlist.txt").read.split("\n")
 is_int = false
 while(is_int == false)
 	print "Enter word length between 5-20: "
-	mr_input = gets
-	if(Integer(mr_input) == false)
-		mr_input = false
-	elsif(mr_input.to_i >= 20)
-		mr_input = 20
+	w_length = gets
+	if(/[[:alpha:]]/.match(w_length))
+		is_int = false
+	elsif(w_length.to_i >= 20) 
+		w_length = 20
 		is_int = true
-	elsif (mr_input.to_i <= 5)
-		mr_input = 5
+	elsif (w_length.to_i <= 5)
+		w_length = 5
+		is_int = true
+	elsif (w_length.to_i >= 5 && w_length.to_i <= 20)#/[5-20]/.match(w_length)
 		is_int = true
 	end
 end
-		
+dictionary.each{|i|
+	if(i.length == w_length)
+		puts " wordL "+i.length + "enterdL "+w_length
+		dictionary.delete(i) #use.compact! to remove nills
+	end
+}
+
+while(num_of_attempts != 0)		
+	print "enter your guess:"
+	cur_guess = gets
+
+	guess_array.push(cur_guess)
 
 
-
-
-
-puts mr_input 
-print "enter your guess:"
+end
