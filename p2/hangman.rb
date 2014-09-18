c33f9bb4e191550210c91398cc3069995ffd0376
swhitsett <kongzero32@gmail.com>
2014-09-18 00:47:08 -0700

    need to insert guess into blank

--- p2/hangman.rb
+++ p2/hangman.rb
@@ -1,12 +1,19 @@
 #!/usr/local/bin/ruby
 
-# need to improt wordlist
-# 
-
-# create a functuon to remove words from this array.
-# to choose a word from the dictionary 
 num_of_attempts = 10
 guess_array = Array.new
+# def start_fair_game(tmp_array)
+# 	word = tmp_array[rand(tmp_array.length)]
+# end
+
+def cheat_function (swap_array, cur_guess)
+	tmp_array = swap_array
+	
+	swap_array.delete_if{ |i|
+		i.include?(cur_guess)
+	}
+	return tmp_array
+end
 
 dictionary =File.open("wordlist.txt").read.split("\n")
 is_int = false
@@ -25,36 +32,36 @@ while(is_int == false)
 		is_int = true
 	end
 end
+
+#---------------------------------------------------------------------------
 # this seems more pratical but wayyyy slower.
 # dictionary.delete_if{|i|
 # 	i.length != w_length.to_i
 # }
 # dictionary.compact
-newArray = Array.new
+swap_array = Array.new
 dictionary.each{|i|
 	if(i.length == w_length.to_i)
-		newArray.push(i)
+		swap_array.push(i)
 	end
 }
-dictionary = newArray
-
+dictionary = swap_array
+#-------------------------------------------------------------------------
 desired_word_length = Array.new(w_length.to_i,'_'.chomp) # WHY IS THERE ALWAYS NEWLINES IN AN ARRAY???
 while(num_of_attempts != 0)		
 
 	print "enter your guess: "
-	cur_guess = gets
+	cur_guess = gets.chomp
 	print "\n"
-	guess_array.push(cur_guess.chomp) 
 
-	#-------------------reading and deleting the word from the array------------------
-	# ?????????????why dosent this work?????????
-	# dictionary.each{|i|
-	# if(cur_guess.include? i)
-	# 	dictionary.pop(i)
-	# end
-	# }
-	# dictionary.compact
-	#--------------------------------------------------------------------------------
+	if((/[[:alpha:]]/.match(cur_guess)))
+		mr_temp = cheat_function(swap_array, cur_guess)
+		guess_array.push(cur_guess)
+	end
+	if (swap_array.length == 0)
+		mr_word = mr_temp[rand(mr_temp.length)]
+		desired_word_length[mr_word.index('y')] = cur_guess
+	end
 
 	guess_array.each{|index| print index + " "}
 	puts " (" + num_of_attempts.to_s + " chances left)"
@@ -62,3 +69,4 @@ while(num_of_attempts != 0)
 	print "\n" + "\n"
 	num_of_attempts = num_of_attempts - 1
 end
+#---------------------------------------------------------------------------
